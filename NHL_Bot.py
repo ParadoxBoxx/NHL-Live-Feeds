165598e3790a405c5194f602232d34391d80e8ee
ParadoxBoxx <LucSkywalker16@gmail.com>
2020-07-20 23:34:39 -0400

    Rewrite - Dropped save states to simplify.

--- NHL_Bot.py
+++ NHL_Bot.py
@@ -1,235 +1,232 @@
 import discord
-from discord.ext import commands
 import aiohttp
 import asyncio
 import json
 import datetime
 
+BASE_API_URL = "https://statsapi.web.nhl.com"
 LIVE_GAME_URL = "https://statsapi.web.nhl.com/api/v1/game/{0}/feed/live"
-SCHEDULE_URL = "https://statsapi.web.nhl.com/api/v1/schedule"
+SCHEDULE_URL = "https://statsapi.web.nhl.com/api/v1/schedule?date=2020-01-02"
 TEAM_URL = "https://statsapi.web.nhl.com/api/v1/teams/{0}"
+TEAM_COLOR = [
+    0Xffffff, # None
+    0xcc0b2b, # NJD
+    0x085399, # NYI
+    0x0639a6, # NYR
+    0xf85f25, # PHI
+    0xfcb631, # PIT
+    0xfbb42d, # BOS
+    0x022753, # BUF
+    0xbf022b, # MTL
+    0xe00231, # OTT
+    0x043f7b, # TOR
+    0Xffffff, # None 
+    0xcc0b2b, # CAR
+    0xe3133d, # FLA
+    0x073f7e, # TBL
+    0xe1002a, # WSH
+    0xd30031, # CHI
+    0xe0002d, # DET
+    0xfeb731, # NSH
+    0x075399, # STL
+    0xe10026, # CGY
+    0x872433, # COL
+    0xf36631, # EDM
+    0x022e55, # VAN
+    0x111111, # ANA
+    0x086a4e, # DAL
+    0x181415, # LAK
+    0Xffffff, # None
+    0x0a6c80, # SJS
+    0x022653, # CBJ
+    0x064932, # MIN
+    0Xffffff, # 31
+    0Xffffff, # 32
+    0Xffffff, # 33
+    0Xffffff, # 34
+    0Xffffff, # 35
+    0Xffffff, # 36
+    0Xffffff, # 37
+    0Xffffff, # 38
+    0Xffffff, # 39
+    0Xffffff, # 40
+    0Xffffff, # 41
+    0Xffffff, # 42
+    0Xffffff, # 43
+    0Xffffff, # 44
+    0Xffffff, # 45
+    0Xffffff, # 46
+    0Xffffff, # 47
+    0Xffffff, # 48
+    0Xffffff, # 49
+    0Xffffff, # 50
+    0Xffffff, # 51
+    0x051e40, # WPG
+    0x8b2534, # PHX
+    0x303e47 # VGK
+]
 
-client = commands.Bot(command_prefix="!")
+client = discord.Client()
 data = {}
+# conn = aiohttp.TCPConnector(limit=1)
+session = aiohttp.ClientSession()
 
-
-async def no_data():
-
-    # Loop forever
+async def return_url_as_json(url_string):
+    global session
     while True:
-
-        # Delete all irelevant game data
-        games_category = discord.utils.get(client.get_all_channels(), name="Games", guild__name="NHL Live Feeds")
-        for old_channel in games_category.channels:
-            await old_channel.delete()
-
-        # Set up a session to get the schedule
-        conn = aiohttp.TCPConnector(limit=1)
-        async with aiohttp.ClientSession(connector=conn) as session:
-            # Get today's schedule
-            schedule_json = await return_url_as_json(session, SCHEDULE_URL)
-        
-            # Check to make sure there are games today
-            if len(schedule_json["dates"]) > 0:
-                
-                # for every game on the schedule today
-                for game in schedule_json["dates"][0]["games"]:
-                    
-                    # Create the discord channel
-                    home_json = await return_url_as_json(session, TEAM_URL.format(str(game["teams"]["home"]["team"]["id"])))
-                    home = home_json["teams"][0]
-                    away_json = await return_url_as_json(session, TEAM_URL.format(str(game["teams"]["away"]["team"]["id"])))
-                    away = away_json["teams"][0]
-                    new_game_channel = await games_category.create_text_channel(away["abbreviation"] + " at " + home["abbreviation"])
-
-                    # Create the game object
-                    # Check to make sure the game doesn't already exist
-                    if str(game["gamePk"]) not in data:
-                        data[str(game["gamePk"])] = {
-                            "NHL_API_ID": game["gamePk"],
-                            "discord_channel_obj": new_game_channel,
-                            "home": home,
-                            "away": away,
-                            "plays": {},
-                            "complete": False,
-                            "datetime_to_delete": datetime.datetime(1970,1,1,0,0,0)
-                        }
-                    else:
-                        # The game already existed, spit out an error
-                        print("DUPLICATE GAME!!!")
-
-                    # Start monitoring for that game
-                    client.loop.create_task(monitor(str(game["gamePk"])))
-
-        # Wait until tomorrow, and start process over again
-        today = datetime.datetime.now()
-        wait_time = datetime.datetime(today.year,today.month, today.day + 1, 12,5,00)
-        await asyncio.sleep((wait_time - datetime.datetime.now()).total_seconds())
-
-
-async def monitor(gamePk):
-    
-    # This needs to coninually happen, we will break out of the loop when the game is over
-    while True:
-
-        #print("Checking " + gamePk)
-        # Get the plays that have happened
-        # Set up a session
-        conn = aiohttp.TCPConnector(limit=1)
-        async with aiohttp.ClientSession(connector=conn) as session:
-            plays_json = await return_url_as_json(session, LIVE_GAME_URL.format(gamePk))
-
-        # For every play that has been returned
-        for event in plays_json["liveData"]["plays"]["allPlays"]:
-
-            # Has this play already been logged?
-            if event["about"]["eventIdx"] in data[gamePk]["plays"]:
-
-                # Already exists so update the play if necessary
-                # play already exists, update any necessary info
-                if data[gamePk]["plays"][event["about"]["eventIdx"]]["eventTypeId"] != event["result"]["eventTypeId"]:
-
-                    # Checking to see if the eventTypeId has changed
-                    data[gamePk]["plays"][event["about"]["eventIdx"]]["eventTypeId"] = event["result"]["eventTypeId"]
-                    data[gamePk]["plays"][event["about"]["eventIdx"]]["posted"] = "edit"
-
-                if data[gamePk]["plays"][event["about"]["eventIdx"]]["description"] != event["result"]["description"]:
-
-                    # Checking to see if the description has changed
-                    data[gamePk]["plays"][event["about"]["eventIdx"]]["description"] = event["result"]["description"]
-                    data[gamePk]["plays"][event["about"]["eventIdx"]]["posted"] = "edit"
-
-            else:
-                # play doesn't exist, add it
-                data[gamePk]["plays"][event["about"]["eventIdx"]] = {
-                    "eventIdx": event["about"]["eventIdx"],
-                    "eventTypeId": event["result"]["eventTypeId"],
-                    "description": event["result"]["description"],
-                    "discord_message_id": -1,
-                    "posted": "new"
-                }
-        
-        # Now that we've updated our internal memory, post/update necessary plays         
-        # For every play that we have in our memory
-        for i in range(len(data[gamePk]["plays"])):
-            
-            # Is the play new or posted?
-            if data[gamePk]["plays"][i]["posted"] == "new":
-
-                # Play is new, post it
-                new_message = await data[gamePk]["discord_channel_obj"].send(data[gamePk]["plays"][i]["description"])
-                data[gamePk]["plays"][i]["discord_message_obj"] = new_message
-                data[gamePk]["plays"][i]["posted"] = "posted"
-            
-            elif data[gamePk]["plays"][i]["posted"] == "edit":
-
-                # Update the play
-                await data[gamePk]["plays"][i]["discord_message_obj"].edit(content=data[gamePk]["plays"][i]["description"])
-                data[gamePk]["plays"][i]["posted"] = "posted"
-    
-        # Now that all the plays are posted, check to see if the game is over
-        if plays_json["gameData"]["status"]["detailedState"] == "Final":
-
-            # Game is over
-            # If this is the first time the game has been reported as "over", set up the deletion time.
-            if data[gamePk]["datetime_to_delete"] == datetime.datetime(1970,1,1,0,0,0):
-
-                # Datetime to delete isn't set, so therefore we must calculate it
-                data[gamePk]["datetime_to_delete"] = datetime.datetime.now() + datetime.timedelta(hours=1)
-
-            # Sleep for the allotted time
-            await asyncio.sleep((data[gamePk]["datetime_to_delete"] - datetime.datetime.now()).total_seconds())
-
-            # Delete the channel
-            await data[gamePk]["discord_channel_obj"].delete()
-
-            # Remove from dictionary
-            data.pop(gamePk)
-
-            # Break out of loop
-            break
-        
-        else:
-            # Game isn't over, sleep and poll again
-            await asyncio.sleep(10)
-
-
-async def return_url_as_json(session_obj, url):
-    while True:
-        async with session_obj.get(url) as response:
-            print("[" + str(response.status) + "] " + url)
+        async with session.get(url_string) as response:
             if response.status != 200:
-                print("Didn't get a 200, sleeping.")
+                log_time = datetime.datetime.now()
+                print("[" + str(log_time.year) + "-" + str(log_time.month) + "-" + str(log_time.day) + " " + str(log_time.hour) + ":" + str(log_time.minute) + ":" + str(log_time.second) + "] Didn't get a 200, sleeping.")
                 await asyncio.sleep(10)
             else:
                 return json.loads(await response.text())
 
+async def load_data():
+    global data
+    try:
+        with open("bot.data", "r") as f:
+            data = json.loads(f.read())
+    except FileNotFoundError as e:
+        pass
+
+    client.loop.create_task(forever_loop())
+
+
+def create_embed(play_obj):
+    if play_obj["eventTypeId"] == "SHOT":
+        embed = discord.Embed(title="Shot", description=play_obj["description"], color=TEAM_COLOR[play_obj["team"]["id"]])
+        embed.set_author(name=play_obj["team"]["triCode"])
+        embed.set_thumbnail(url=BASE_API_URL + play_obj["players"][0]["player"]["link"])
+        return embed
+    else:
+        return discord.Embed(title="PLACEHOLDER", description=play_obj["description"])
+    
+
+async def forever_loop():
+    global data
+    global session
+    global client
+
+    while True:
+        games_category = discord.utils.get(client.get_all_channels(), name="Games", guild__name="NHL Live Feeds")
+        if not data:
+            # No data, get the schedule
+            schedule_json = await return_url_as_json(SCHEDULE_URL)
+            for old_channel in games_category.channels:
+                await old_channel.delete()
+
+        if schedule_json["totalGames"] <= 0:
+            log_time = datetime.datetime.now()
+            print("[" + str(log_time.year) + "-" + str(log_time.month) + "-" + str(log_time.day) + " " + str(log_time.hour) + ":" + str(log_time.minute) + ":" + str(log_time.second) + "] No games today.")
+        elif schedule_json["totalGames"] > 0:
+            for game in schedule_json["dates"][0]["games"]:
+                home_json = await return_url_as_json(TEAM_URL.format(str(game["teams"]["home"]["team"]["id"])))
+                away_json = await return_url_as_json(TEAM_URL.format(str(game["teams"]["away"]["team"]["id"])))
+                new_game_channel = await games_category.create_text_channel(away_json["teams"][0]["abbreviation"] + " at " + home_json["teams"][0]["abbreviation"])
+                data[str(game["gamePk"])] = {
+                    "gamePk": game["gamePk"],
+                    "gameDateTime": datetime.datetime.strptime(game["gameDate"], "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=datetime.timezone.utc),
+                    "home": {
+                        "id": game["teams"]["home"]["team"]["id"],
+                        "name": game["teams"]["home"]["team"]["name"],
+                        "abbreviation": home_json["teams"][0]["abbreviation"]
+                    },
+                    "away": {
+                        "id": game["teams"]["away"]["team"]["id"],
+                        "name": game["teams"]["away"]["team"]["name"],
+                        "abbreviation": away_json["teams"][0]["abbreviation"]
+                    },
+                    "discord_channel": new_game_channel,
+                    "plays": {}
+                }
+
+            for gpk in data:
+                client.loop.create_task(monitor_game(gpk))
+
+        now = datetime.datetime.now()
+        tomorrow = now + datetime.timedelta(days=1)
+        tomorrow = tomorrow.replace(hour=12, minute=5, second=0, microsecond=0)
+        log_time = datetime.datetime.now()
+        print("[" + str(log_time.year) + "-" + str(log_time.month) + "-" + str(log_time.day) + " " + str(log_time.hour) + ":" + str(log_time.minute) + ":" + str(log_time.second) +
+        "] main loop sleeping " + str((tomorrow - now).total_seconds()) + " seconds.")
+        await asyncio.sleep((tomorrow - now).total_seconds())
+
+
+async def monitor_game(gamePk):
+    global data
+
+    now = datetime.datetime.now().astimezone(datetime.timezone.utc)
+    if now < data[gamePk]["gameDateTime"]:
+        asyncio.sleep((data[gamePk]["gameDateTime"] - now).total_seconds())
+    
+    game_complete = False
+
+    while not game_complete:
+        log_time = datetime.datetime.now()
+        print("[" + str(log_time.year) + "-" + str(log_time.month) + "-" + str(log_time.day) + " " + str(log_time.hour) + ":" + str(log_time.minute) + ":" + str(log_time.second) +
+        "] Checking for update for {} at {}".format(data[gamePk]["away"]["abbreviation"], data[gamePk]["home"]["abbreviation"]))
+        live_json = await return_url_as_json(LIVE_GAME_URL.format(str(gamePk)))
+
+        # See what new plays we have as well as update preexisting plays
+        for event in live_json["liveData"]["plays"]["allPlays"]:
+            if event["about"]["eventIdx"] not in data[gamePk]["plays"]:
+                try:
+                    data[gamePk]["plays"][event["about"]["eventIdx"]] = {
+                        "eventIdx": event["about"]["eventIdx"],
+                        "eventTypeId": event["result"]["eventTypeId"],
+                        "description": event["result"]["description"],
+                        "players": event["players"],
+                        "team": event["team"],
+                        "discord_message_id": -1,
+                        "posted": "new"
+                    }
+                except KeyError:
+                    data[gamePk]["plays"][event["about"]["eventIdx"]] = {
+                        "eventIdx": event["about"]["eventIdx"],
+                        "eventTypeId": event["result"]["eventTypeId"],
+                        "description": event["result"]["description"],
+                        "players": None,
+                        "team": None,
+                        "discord_message_id": -1,
+                        "posted": "new"
+                    }
+            else:
+                if event["result"]["eventTypeId"] != data[gamePk]["plays"][event["about"]["eventIdx"]]["eventTypeId"]:
+                    data[gamePk]["plays"][event["about"]["eventIdx"]]["eventTypeId"] = event["result"]["eventTypeId"]
+                    data[gamePk]["plays"][event["about"]["eventIdx"]]["posted"] = "edited"
+                if event["result"]["description"] != data[gamePk]["plays"][event["about"]["eventIdx"]]["description"]:
+                    data[gamePk]["plays"][event["about"]["eventIdx"]]["description"] = event["result"]["description"]
+                    data[gamePk]["plays"][event["about"]["eventIdx"]]["posted"] = "edited"
+
+        # Now that our internal data model is updated, we need to update discord as necessary.
+        for playIdx in data[gamePk]["plays"]:
+            if data[gamePk]["plays"][playIdx]["posted"] is "new":
+                discord_message_obj = await data[gamePk]["discord_channel"].send(embed=create_embed(data[gamePk]["plays"][playIdx]))
+                data[gamePk]["plays"][playIdx]["discord_message_id"] = discord_message_obj
+                data[gamePk]["plays"][playIdx]["posted"] = "posted"
+            elif data[gamePk]["plays"][playIdx]["posted"] is "edited":
+                discord_message_obj.edit(data[gamePk]["plays"][playIdx]["description"])
+                data[gamePk]["plays"][playIdx]["posted"] = "posted"
+
+        if live_json["gameData"]["status"]["codedGameState"] is "7":
+            #TODO: Verify if code 7 always means complete
+            log_time = datetime.datetime.now()
+            print("[" + str(log_time.year) + "-" + str(log_time.month) + "-" + str(log_time.day) + " " + str(log_time.hour) + ":" + str(log_time.minute) + ":" + str(log_time.second) +
+            "] {} at {} is complete, deleting in one hour.".format(data[gamePk]["away"]["abbreviation"], data[gamePk]["home"]["abbreviation"]))
+            await asyncio.sleep(3600)
+            await data[gamePk]["discord_channel"].delete()
+            data.pop(gamePk)
+            game_complete = True
+        else:
+            await asyncio.sleep(1)
+
 
 @client.event
 async def on_ready():
-    
-    print("READY!")
-    client.loop.create_task(no_data())
+    log_time = datetime.datetime.now()
+    print("[" + str(log_time.year) + "-" + str(log_time.month) + "-" + str(log_time.day) + " " + str(log_time.hour) + ":" + str(log_time.minute) + ":" + str(log_time.second) + "] Discord Ready")
+    client.loop.create_task(load_data())
 
-
-async def one():
-    embed = discord.Embed(
-        title=":rotating_light: Goal! :rotating_light: ",
-        description="Sably Crabsby sets up Alfredo Mankin for the super shot.",
-        color=discord.Color.red()
-    )
-
-    embed.add_field(
-        name="Picksberg Peanuts",
-        value="7",
-        inline=True
-    )
-    embed.add_field(
-        name="a",
-        value="vs",
-        inline=True
-    )
-    embed.add_field(
-        name="Sun Mojave Squids",
-        value="1",
-        inline=True
-    )
-
-    return embed
-
-
-async def two():
-    embed = discord.Embed(
-        title=":rotating_light: Goal! :rotating_light: ",
-        description="Sably Crabsby sets up Alfredo Mankin for the super shot.",
-        color=discord.Color.red()
-    )
-
-    embed.add_field(
-        name="Picksberg Peanuts vs Sun Mojave Squids",
-        value="7 - 1"
-    )
-
-    return embed
-
-
-@client.command(pass_context=True)
-async def test(ctx, *arg):
-
-    print(arg[0])
-
-    test_variants = {
-        "1": await one(),
-        "2": await two()
-        }
-    
-    await ctx.send(embed=test_variants[arg[0]])
-
-
-@client.command(pass_context=True)
-async def ping(ctx):
-    await ctx.send("Pong!")
-                
-
-with open("token.data", "r") as f:
+with open("token.data","r") as f:
     client.run(f.read())
